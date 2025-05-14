7358ab400996caef4171aec3019e023ee56a3c5e
Chintan Chavda <chintan1971@yahoo.com>
2025-05-13 21:47:25 -0400

    added rviz2 launch file

---
+++ launch/robot.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import os
+
+def generate_launch_description():
+    package_share = FindPackageShare('my_robot_arm').find("my_robot_arm")
+    urdf_path = os.path.join(package_share, 'urdf', 'arm.urdf')
+
+    with open(urdf_path, 'r') as f:
+        robot_description = f.read()
+
+    return LaunchDescription([
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher'
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}]                        
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
